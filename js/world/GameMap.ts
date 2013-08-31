e6eb88b62515a44cfc0a521cff07cb3cb92aa44c
Kaan <kaan.duran@gmail.com>
2013-08-31 22:23:01 +1000

    Added Structures to GameMap for both village and farm (only the big "larger than 1 tile" structures added at this point)

--- js/world/GameMap.ts
+++ js/world/GameMap.ts
@@ -23,6 +23,9 @@ class MapLink {
 var MAPS:collections.Dictionary<string,Array<Array<number>>>;
 MAPS = new collections.Dictionary<string,Array<Array<number>>>();
 
+var STRUCTURES:collections.Dictionary<string,Array<Structure>>;
+STRUCTURES = new collections.Dictionary<string,Array<Structure>>();
+
 var MAP_TO_MAP : collections.Dictionary<MapLink,MapLink>;
 MAP_TO_MAP = new collections.Dictionary<MapLink,MapLink>();
 
@@ -59,6 +62,17 @@ MAPS[MapType.VillageMap] = [
 '======,,,,,,,,,,,======='
 ];
 
+STRUCTURES[MapType.VillageMap] = [
+    new Structure ('#background',"Village_Gate",StructureType.NS_Gate,new Point(11,0), new Point(10,0), new Point(12,0)),
+    new Structure ('#background',"Farm1",StructureType.EF_StrawHouse,new Point(5,6), new Point(3,5), new Point(5,7)),
+    new Structure ('#background',"Farm2",StructureType.WF_StrawHouse,new Point(16,6), new Point(16,5), new Point(18,7)),
+    new Structure ('#background',"Snorri the Sage",StructureType.EF_Hut,new Point(7,12), new Point(8,12), new Point(8,13)),
+    new Structure ('#background',"Olaf's Junk Store",StructureType.WF_StrawHouse,new Point(14,12), new Point(14,11), new Point(16,13)),
+    new Structure ('#background',"Bjorn the Blacksmith",StructureType.EF_StrawHouse,new Point(6,17), new Point(8,16), new Point(8,18)),
+    new Structure ('#background',"Gunnhild's General Store",StructureType.WF_StrawHouse,new Point(14,17), new Point(14,16), new Point(16,18)),
+    new Structure ('#background',"Shrine of Odin",StructureType.NF_HutTemple,new Point(11,21), new Point(9,21), new Point(13,25))
+];
+
 MAPS[MapType.FarmMap] = [
     '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^',
     '^^^^^^^^^^^^^^^^^^^^^^^^+^^^^^^^^^^^^^^^^^^^^^^^^',
@@ -93,4 +107,8 @@ MAPS[MapType.FarmMap] = [
     '========,,,.;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,=======',
     '========,,,.,,,,,=======,,,,,,,,,,,,,,,,,,=======',
     '========,,#+#,,,,=======,,,,,,,,,,,,,,,,,,,,,,,,,'
+];
+
+STRUCTURES[MapType.FarmMap] = [
+    new Structure ('#background',"HeroBurntFarm",StructureType.WF_BurntStrawHouse,new Point(43,24), new Point(43,22), new Point(46,25))
 ];
