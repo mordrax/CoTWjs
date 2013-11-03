bf82364f2ce48cabe87ea3072cf641cee67322c2
joe <joseph.ni>
2013-11-03 17:18:01 +1100

    updated TODO list

--- js/TODO.ts
+++ js/TODO.ts
@@ -1,34 +1,97 @@
 /**
+ * Priorities - 1-Highest, 5-Lowest
+ *
  * TODOs:
  *
  * Inventory:
- * Limit what can go into each equipment slot
- * Double click to open/close items with containers
- * Limit container contents
- * - (weight, bulk, quantity e.g belt slot, cannot put into itself)
- * - weight, bulk
- * - cost
- * - imbue effects
- * - merging
- * - belt, activate menu
- * Opening bags only hooked onto items created before the open inventory screen
-
+ * 4 Limit what can go into each equipment slot
+ * 3 Limit container contents
+ *      - (weight, bulk, quantity e.g belt slot, cannot put into itself)
+ *      - weight, bulk
+ * 4 Belt, update activate menu
+ * 1 Opening bags only hooked onto items created before the open inventory screen
+ * 5 merge coins
+ * DONE - equiping items
+ * DONE - Double click to open/close items with containers
+ *
+ * Shops:
+ * 2 buy/sell
+ * 4 identify items services
+ * 3 generate all shops
+ * 4 - Bjorn sells broken swords, and too good items (Hint: goodsQuality)
+ *
  * Monsters:
+ * 2 monster stats
+ *      - att,
+ *      - def,
+ * 2 monster drop loot
+ * 4 monster special attacks
+ *      - ghosts/spectre monsters (move through walls)
+ *      - dragons (cast spells)
+ *      - giants (throws boulders)
+ *      - thieves (steal stuff)
  * DONE - Monster move alternatives
  * DONE - Monster hit hero on structure
  *
  * World:
- * Tiles hold containers and can open inventory
- * Solid tiles
+ * 1 Solid tiles (caves, crops, crap)
+ * DONE - Tiles hold containers and can open inventory
+ *
+ * Story:
+ * 5 burnt out farm
+ * 5 note on dungeon level last
+ * 5 burnt out village
  *
  * Dungeon:
- * Dungeon generation
- * Stair maplinks
- * Corner walls
+ * 2 Dungeon generation
+ *      - Stair maplinks
+ * 4 dungeon floor misalign graphical glitch
+ * DONE - Corner walls
  *
  * HUD:
- * Hero status view
+ * ACTIVE - Hero status view
+ * 3 buttons
+ *      - Get,
+ *      - Free Hand,
+ *      - Search
+ * 4 spell buttons
  *
- * Combat system
- * weapon stats, armour stats, monster stas
+ * Menu:
+ * file menu
+ * char menu
+ * DONE - inventory menu
+ * map menu
+ * spell menu
+ * activate - freehand + belt
+ * verbs
+ * - rest,
+ * - sleep,
+ * - open/close,
+ * - disarm trap,
+ * - look,
+ * - climb
+ *
+ * Hotkeys:
+ * 2 c, i, etc...
+ *
+ * Combat system:
+ * 2 basic weapon/armour stats, monster stas
+ * 4 fog of war
+ * 4 visible sight
+ * 3 speed
+ *      - walking
+ *      - fighting
+ * 3 dying
+ * 
+ * Spell system:
+ * 5 everything
+ *
+ * Game:
+ * 5 new game
+ * 3 char creation
+ * 5 save/load game
+ *
+ * Game concepts:
+ * 5 identifyable items
+ *
  */
